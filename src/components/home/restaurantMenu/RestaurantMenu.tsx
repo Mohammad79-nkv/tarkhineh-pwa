7f0d759615455838619fe3709bda3a18b98fac64
mohammad <mohammadnv79@gmail.com>
2023-05-08 21:48:23 +0330

    add RestaurantMenu

--- src/components/home/restaurantMenu/RestaurantMenu.tsx
+++ src/components/home/restaurantMenu/RestaurantMenu.tsx
@@ -1,19 +1,33 @@
+import Image from "next/image";
+
 const RestaurantMenu = () => {
   const menuData = [
-    { id: 1, name: "نوشیدنی", imageSrc: "", route: "" },
-    { id: 2, name: "نوشیدنی", imageSrc: "", route: "" },
-    { id: 3, name: "نوشیدنی", imageSrc: "", route: "" },
-    { id: 4, name: "نوشیدنی", imageSrc: "", route: "" },
+    { id: 1, name: "نوشیدنی", imageSrc: "/images/drink.png", route: "" },
+    { id: 2, name: "دسر", imageSrc: "/images/dissert.png", route: "" },
+    { id: 3, name: "پیش غذا", imageSrc: "/images/preFood.png", route: "" },
+    { id: 4, name: "غذای اصلی", imageSrc: "/images/mainFood.png", route: "" },
   ];
 
   return (
     <section>
-      ]<p></p>
-      <div className="flex justify-between px-[7vw] gap-6">
+      <p className="text-center text-2xl font-bold	">منوی رستوران</p>
+      <div className="flex justify-between px-[7vw] gap-6 h-[180px] mt-32">
         {menuData.map((item) => (
-          <div key={item.id} className="flex flex-col items-center justify-center relative border-primary shadow-cards-shadow border w-full rounded-8">
-            dwadad
-            <button className="absolute bottom-[-20px] bg-primary p-2 text-white shadow-cards-shadow rounded-4">{item.name}</button>
+          <div
+            key={item.id}
+            className="h-full flex flex-col items-center justify-center relative border-primary shadow-cards-shadow border w-full rounded-8"
+          >
+            <div className="absolute top-[-100px]">
+              <Image
+                src={item.imageSrc}
+                alt={item.name}
+                width="200"
+                height="200"
+              />
+            </div>
+            <button className="absolute bottom-[-20px] bg-primary p-2 text-white shadow-cards-shadow rounded-4 h-12 w-36 text-xl">
+              {item.name}
+            </button>
           </div>
         ))}
       </div>
