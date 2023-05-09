276f1c28181f565ee953a4dc237b917532563fd9
Mohammad79-nkv <82023099+Mohammad79-nkv@users.noreply.github.com>
2023-05-09 04:01:50 +0000

    update service worker

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      handler: 'CacheFirst',
+      urlPattern: /^https?.*/,
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+        },
+      }
+    }
+  ]
 });
 
 module.exports = withPWA({
