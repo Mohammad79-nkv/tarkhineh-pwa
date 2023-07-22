c70e835704356ea0acf9f541687fe3b144463ce4
mohammad <mohammadnv79@gmail.com>
2023-07-22 08:12:01 +0330

    comment axios config

--- src/services/_base-service.ts
+++ src/services/_base-service.ts
@@ -1,185 +1,185 @@
-// library
-import Axios, {
-  AxiosInstance,
-  AxiosRequestConfig,
-  CancelTokenSource,
-} from "axios";
-// custom
-import { store } from "./../redux/store";
-import axiosRetry from "axios-retry";
-import { toast } from "react-toastify";
-import { getRefreshToken } from "./../redux/slices/auth";
+// // library
+// import Axios, {
+//   AxiosInstance,
+//   AxiosRequestConfig,
+//   CancelTokenSource,
+// } from "axios";
+// // custom
+// import { store } from "./../redux/store";
+// import axiosRetry from "axios-retry";
+// import { toast } from "react-toastify";
+// import { getRefreshToken } from "./../redux/slices/auth";
 
-const abortController = new AbortController();
+// const abortController = new AbortController();
 
-export const cancelApi = () => {
-  abortController.abort();
-};
+// export const cancelApi = () => {
+//   abortController.abort();
+// };
 
-const retryDelay = (retryNumber = 0) => {
-  const seconds = Math.pow(2, retryNumber) * 1000;
-  const randomMs = 1000 * Math.random();
-  return seconds + randomMs;
-};
+// const retryDelay = (retryNumber = 0) => {
+//   const seconds = Math.pow(2, retryNumber) * 1000;
+//   const randomMs = 1000 * Math.random();
+//   return seconds + randomMs;
+// };
 
-export abstract class BaseService {
-  constructor(alternativeBaseUrl?: string) {
-    this.axiosRequestConfig = {
-      baseURL: alternativeBaseUrl
-        ? alternativeBaseUrl
-        : (import.meta.env.VITE_APP_BASE_URL as string),
-    };
-    this.axiosInstance = Axios.create(this.axiosRequestConfig);
-    axiosRetry(this.axiosInstance, {
-      retries: 2,
-      retryDelay,
-      retryCondition: axiosRetry.isRetryableError,
-    });
-  }
+// export abstract class BaseService {
+//   constructor(alternativeBaseUrl?: string) {
+//     this.axiosRequestConfig = {
+//       baseURL: alternativeBaseUrl
+//         ? alternativeBaseUrl
+//         : (import.meta.env.VITE_APP_BASE_URL as string),
+//     };
+//     this.axiosInstance = Axios.create(this.axiosRequestConfig);
+//     axiosRetry(this.axiosInstance, {
+//       retries: 2,
+//       retryDelay,
+//       retryCondition: axiosRetry.isRetryableError,
+//     });
+//   }
 
-  baseUrl: string = "";
+//   baseUrl: string = "";
 
-  private static token: string | null = null;
-  protected axiosInstance: AxiosInstance;
-  protected axiosRequestConfigDefault: AxiosRequestConfig = {
-    baseURL: this.baseUrl,
-    headers: { "Content-Type": "application/json" },
-  };
+//   private static token: string | null = null;
+//   protected axiosInstance: AxiosInstance;
+//   protected axiosRequestConfigDefault: AxiosRequestConfig = {
+//     baseURL: this.baseUrl,
+//     headers: { "Content-Type": "application/json" },
+//   };
 
-  private _axiosRequestConfig: AxiosRequestConfig =
-    this.axiosRequestConfigDefault;
+//   private _axiosRequestConfig: AxiosRequestConfig =
+//     this.axiosRequestConfigDefault;
 
-  get axiosRequestConfig() {
-    return this._axiosRequestConfig;
-  }
+//   get axiosRequestConfig() {
+//     return this._axiosRequestConfig;
+//   }
 
-  set axiosRequestConfig(config: any) {
-    if (config.headers) {
-      config.headers = {
-        ...this._axiosRequestConfig.headers,
-        ...config.headers,
-      };
-    }
-    this._axiosRequestConfig = { ...this._axiosRequestConfig, ...config };
-  }
+//   set axiosRequestConfig(config: any) {
+//     if (config.headers) {
+//       config.headers = {
+//         ...this._axiosRequestConfig.headers,
+//         ...config.headers,
+//       };
+//     }
+//     this._axiosRequestConfig = { ...this._axiosRequestConfig, ...config };
+//   }
 
-  get axiosInstanceWithoutToken(): AxiosInstance {
-    let axiosInstanceWithoutToken: AxiosInstance = this.axiosInstance;
+//   get axiosInstanceWithoutToken(): AxiosInstance {
+//     let axiosInstanceWithoutToken: AxiosInstance = this.axiosInstance;
 
-    axiosInstanceWithoutToken.interceptors.response.use(
-      function (response: any) {
-        // Any status code that lie within the range of 2xx cause this function to trigger
-        // Do something with response data
-        return response;
-      },
-      function (error: any) {
-        toast.error(error.message);
-        return Promise.reject(error);
-      }
-    );
+//     axiosInstanceWithoutToken.interceptors.response.use(
+//       function (response: any) {
+//         // Any status code that lie within the range of 2xx cause this function to trigger
+//         // Do something with response data
+//         return response;
+//       },
+//       function (error: any) {
+//         toast.error(error.message);
+//         return Promise.reject(error);
+//       }
+//     );
 
-    return axiosInstanceWithoutToken;
-  }
+//     return axiosInstanceWithoutToken;
+//   }
 
-  get axiosInstanceWithToken(): AxiosInstance {
-    let axiosInstanceWithToken: AxiosInstance;
+//   get axiosInstanceWithToken(): AxiosInstance {
+//     let axiosInstanceWithToken: AxiosInstance;
 
-    axiosInstanceWithToken = Axios.create();
+//     axiosInstanceWithToken = Axios.create();
 
-    axiosInstanceWithToken.interceptors.request.use((config: any) => {
-      const {
-        token: { access_token },
-      } = store.getState().auth;
-      config.baseURL = process.env.VITE_APP_BASE_URL;
-      config.headers = {
-        "Content-Type": "application/json",
-        "X-VERSION": import.meta.env.VITE_APP_VERSION,
-        Authorization: "Bearer " + access_token,
-      };
-      return config;
-    });
+//     axiosInstanceWithToken.interceptors.request.use((config: any) => {
+//       const {
+//         token: { access_token },
+//       } = store.getState().auth;
+//       config.baseURL = process.env.VITE_APP_BASE_URL;
+//       config.headers = {
+//         "Content-Type": "application/json",
+//         "X-VERSION": import.meta.env.VITE_APP_VERSION,
+//         Authorization: "Bearer " + access_token,
+//       };
+//       return config;
+//     });
 
-    axiosInstanceWithToken.interceptors.response.use(
-      function (response: any) {
-        return response;
-      },
-      async function (error: any) {
-        const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._retry) {
-          originalRequest._retry = true;
-          const refreshToken = store.getState().auth.token.refresh_token; /// fetching Refresh
-          const axiosForRefresh = Axios.create({
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-              Authorization: "Basic d2ViOg==",
-            },
-          });
-          const params = new URLSearchParams();
-          params.append("grant_type", "refresh_token");
-          params.append("refresh_token", refreshToken ?? "");
+//     axiosInstanceWithToken.interceptors.response.use(
+//       function (response: any) {
+//         return response;
+//       },
+//       async function (error: any) {
+//         const originalRequest = error.config;
+//         if (error.response.status === 401 && !originalRequest._retry) {
+//           originalRequest._retry = true;
+//           const refreshToken = store.getState().auth.token.refresh_token; /// fetching Refresh
+//           const axiosForRefresh = Axios.create({
+//             headers: {
+//               "Content-Type": "application/x-www-form-urlencoded",
+//               Authorization: "Basic d2ViOg==",
+//             },
+//           });
+//           const params = new URLSearchParams();
+//           params.append("grant_type", "refresh_token");
+//           params.append("refresh_token", refreshToken ?? "");
 
-          const res = await axiosForRefresh.post(
-            `
-            ${
-              import.meta.env.VITE_APP_AUTH_URL
-            }/realms/smartbss/protocol/openid-connect/token`,
-            params
-          );
+//           const res = await axiosForRefresh.post(
+//             `
+//             ${
+//               import.meta.env.VITE_APP_AUTH_URL
+//             }/realms/smartbss/protocol/openid-connect/token`,
+//             params
+//           );
 
-          store.dispatch(getRefreshToken(res.data)); //// dispatch to redux
-          axiosInstanceWithToken.defaults.headers.common["Authorization"] =
-            "Bearer " + res.data.access_token;
-          return axiosInstanceWithToken(originalRequest);
-        } else {
-          toast.error(error.message);
-        }
-        return Promise.reject(error);
-      }
-    );
-    return axiosInstanceWithToken;
-  }
+//           store.dispatch(getRefreshToken(res.data)); //// dispatch to redux
+//           axiosInstanceWithToken.defaults.headers.common["Authorization"] =
+//             "Bearer " + res.data.access_token;
+//           return axiosInstanceWithToken(originalRequest);
+//         } else {
+//           toast.error(error.message);
+//         }
+//         return Promise.reject(error);
+//       }
+//     );
+//     return axiosInstanceWithToken;
+//   }
 
-  get axiosInstanceWithTokenForLogin(): AxiosInstance {
-    let token = BaseService.token;
-    let axiosInstanceWithToken: AxiosInstance;
-    token = "Basic d2ViOg==";
-    if (token) {
-      this.axiosRequestConfig = {
-        headers: {
-          Authorization: token,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      };
-      axiosInstanceWithToken = Axios.create({
-        ...this.axiosRequestConfig,
-      });
-    } else {
-      axiosInstanceWithToken = this.axiosInstance;
-    }
+//   get axiosInstanceWithTokenForLogin(): AxiosInstance {
+//     let token = BaseService.token;
+//     let axiosInstanceWithToken: AxiosInstance;
+//     token = "Basic d2ViOg==";
+//     if (token) {
+//       this.axiosRequestConfig = {
+//         headers: {
+//           Authorization: token,
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//       };
+//       axiosInstanceWithToken = Axios.create({
+//         ...this.axiosRequestConfig,
+//       });
+//     } else {
+//       axiosInstanceWithToken = this.axiosInstance;
+//     }
 
-    axiosInstanceWithToken.interceptors.response.use(
-      function (response: any) {
-        return response;
-      },
-      async function (error: any) {
-        toast.error(error.message);
-        return Promise.reject(error);
-      }
-    );
+//     axiosInstanceWithToken.interceptors.response.use(
+//       function (response: any) {
+//         return response;
+//       },
+//       async function (error: any) {
+//         toast.error(error.message);
+//         return Promise.reject(error);
+//       }
+//     );
 
-    return axiosInstanceWithToken;
-  }
+//     return axiosInstanceWithToken;
+//   }
 
-  // private static setToken(token: string) {
-  //   BaseService.token = token;
-  // }
+//   // private static setToken(token: string) {
+//   //   BaseService.token = token;
+//   // }
 
-  static removeToken() {
-    BaseService.token = null;
-  }
+//   static removeToken() {
+//     BaseService.token = null;
+//   }
 
-  static msgRequestCanceled = "request-canceled";
-  static cancelTokenSource(): CancelTokenSource {
-    return Axios.CancelToken.source();
-  }
-}
+//   static msgRequestCanceled = "request-canceled";
+//   static cancelTokenSource(): CancelTokenSource {
+//     return Axios.CancelToken.source();
+//   }
+// }
--- src/services/_crud-tm-service.ts
+++ src/services/_crud-tm-service.ts
@@ -1,59 +1,59 @@
-import { ApiPayload } from "@/model/service/api-payload.model";
-import { ApiResponse } from "@/model/service/api-response.model";
-import { BaseService } from "./_base-service";
+// import { ApiPayload } from "@/model/service/api-payload.model";
+// import { ApiResponse } from "@/model/service/api-response.model";
+// import { BaseService } from "./_base-service";
 
-export abstract class CrudTmForumService<
-  EntityModel,
-  EntityCreateModel,
-  EntityUpdateModel
-> extends BaseService {
-  abstract entityBaseUrl: string;
-  abstract versionName: string;
-  abstract entityName?: string;
+// export abstract class CrudTmForumService<
+//   EntityModel,
+//   EntityCreateModel,
+//   EntityUpdateModel
+// > extends BaseService {
+//   abstract entityBaseUrl: string;
+//   abstract versionName: string;
+//   abstract entityName?: string;
 
-  create(payload: EntityCreateModel) {
-    return this.axiosInstanceWithToken.post(
-      `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}`,
-      payload
-    );
-  }
+//   create(payload: EntityCreateModel) {
+//     return this.axiosInstanceWithToken.post(
+//       `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}`,
+//       payload
+//     );
+//   }
 
-  update(payload: EntityUpdateModel, entityId: string) {
-    return this.axiosInstanceWithToken.put(
-      `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`,
-      payload
-    );
-  }
+//   update(payload: EntityUpdateModel, entityId: string) {
+//     return this.axiosInstanceWithToken.put(
+//       `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`,
+//       payload
+//     );
+//   }
 
-  updatePatch(payload: EntityUpdateModel, entityId: string | null) {
-    return this.axiosInstanceWithToken.patch(
-      `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`,
-      payload
-    );
-  }
+//   updatePatch(payload: EntityUpdateModel, entityId: string | null) {
+//     return this.axiosInstanceWithToken.patch(
+//       `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`,
+//       payload
+//     );
+//   }
 
-  getList(payload?: ApiPayload): Promise<ApiResponse<Array<EntityModel>>> {
-    return this.axiosInstanceWithToken.get(
-      `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}`,
-      { params: payload }
-    );
-  }
+//   getList(payload?: ApiPayload): Promise<ApiResponse<Array<EntityModel>>> {
+//     return this.axiosInstanceWithToken.get(
+//       `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}`,
+//       { params: payload }
+//     );
+//   }
 
-  getListWithOutToken(): any {
-    return this.axiosInstanceWithoutToken.get(
-      `/${this.entityBaseUrl}`
-    );
-  }
+//   getListWithOutToken(): any {
+//     return this.axiosInstanceWithoutToken.get(
+//       `/${this.entityBaseUrl}`
+//     );
+//   }
 
-  getById(entityId: string): Promise<ApiResponse<EntityModel>> {
-    return this.axiosInstanceWithToken.get(
-      `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`
-    );
-  }
+//   getById(entityId: string): Promise<ApiResponse<EntityModel>> {
+//     return this.axiosInstanceWithToken.get(
+//       `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`
+//     );
+//   }
 
-  delete(entityId: string | undefined) {
-    return this.axiosInstanceWithToken.delete(
-      `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`
-    );
-  }
-}
+//   delete(entityId: string | undefined) {
+//     return this.axiosInstanceWithToken.delete(
+//       `/${this.entityBaseUrl}/${this.versionName}/${this.entityName}/${entityId}`
+//     );
+//   }
+// }
