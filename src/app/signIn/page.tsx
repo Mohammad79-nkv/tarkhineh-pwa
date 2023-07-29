70adb7c489e2c973e894e0801d1355f97c47c725
mohammad <mohammadnv79@gmail.com>
2023-07-29 16:14:19 +0330

    add signIn form

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SimpleLogo } from "@/components/icons/SimpleLoga";
+import StyledInput from "@/components/styledInput/StyledInput";
 import { useForm } from "react-hook-form";
 
 const SignIn = () => {
@@ -15,7 +16,7 @@ const SignIn = () => {
   return (
     <section className="flex justify-center items-center bg-gray-100 h-screen w-screen">
       <div className="w-full h-full bg-white rounded-4 md:shadow md:max-w-md md:max-h-[700px] flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-3">
+        <div className="flex flex-col justify-center items-center gap-3 w-1/2">
           <div className="w-20">
             <SimpleLogo />
           </div>
@@ -24,18 +25,19 @@ const SignIn = () => {
             لطفا برای ثبت نام تمام اطلاعات خواسته شده را وارد کنید{" "}
           </p>
         </div>
-        <div className="">
-          <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="w-1/2">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 mt-4">
             {/* register your input into the hook by invoking the "register" function */}
-            <input  placeholder="dwed" {...register("example")}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+            
+            <StyledInput  placeholder="شماره موبایل " {...register("example")} />
             {errors.e && <span>This field is required</span>}
 
             {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} />
+            <StyledInput  placeholder="رمز عبور" {...register("example")} type="password"/>
             {/* errors will return when field validation fails  */}
             {errors.exampleRequired && <span>This field is required</span>}
 
-            <input type="submit" />
+            <button className="bg-primary text-white text-sm  rounded-4 py-2">تایید و ادامه</button>
           </form>
         </div>
       </div>
